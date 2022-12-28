5a119f8c5203dd0666597145453431d0476c842b
Kyungmo-Lee <leekyungmo12@gmail.com>
2022-12-28 19:01:55 +0900

    chapter1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { legacy_createStore as createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
